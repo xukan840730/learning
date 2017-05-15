b8363047e64cf2a139ce579851e4134a18f5401e
Kan Xu <xukanxukan@gmail.com>
2017-05-14 19:39:17 -0700

    try newton's method to get theta.

--- ml/ml01.py
+++ ml/ml01.py
@@ -1,60 +1,65 @@
 import numpy as np
+import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 
-def log_regression(X, Y, max_iters):
-    mm = X.shape[0]
-    nn = X.shape[1]
-    print mm, nn
+def newton_opt(xa, ya, max_iters):
+    xm = np.matrix(xa)
+    ym = np.matrix(ya)
+
+    mm = xm.shape[0]
+    nn = xm.shape[1]
+
+    theta = np.zeros((nn, 1))
+
+    ll = np.zeros((max_iters, 1))
+
+    ## newton's method to minimize function.
+    for i in range(1, max_iters):
+        margins = np.multiply(ym, xm * theta)
+        ll[i] = (1.0/mm) * np.sum(np.log(1 + np.exp(-margins)))
+        probs = 1.0 / (1 + np.exp(margins))
+        grad = -(1.0/mm) * (xm.transpose() * np.multiply(probs, ym))
+        H = (1.0/mm) * (xm.transpose() * np.diag(np.array(np.multiply(probs, (1-probs))).flatten()) * xm)
+        theta = theta - linalg.inv(H) * grad
+
+    return theta, ll
 
 def main():
     x = np.loadtxt('logistic_x.txt')
-    Y = np.loadtxt('logistic_y.txt')
-
-    #Y = np.reshape(Y, (Y.size, 1))
-
-    #test1()
+    y = np.loadtxt('logistic_y.txt')
 
     t1 = np.ones((x.shape[0], 1))
     X = np.append(t1, x, axis=1)
-    #print X
-    #print X.shape
+    Y = y.reshape((y.size, 1))
 
-    log_regression(X, Y, 20)
+    # theta = np.matrix([[-2.6205, 0.7604, 1.1719]]), # result from anser
+    theta, ll = newton_opt(X, Y, 20)
 
-    theta = np.array([[-2.6205, 0.7604, 1.1719]])
-    #print theta
-    #print theta.shape
-
-    t2 = np.dot(X, theta.transpose())
+    t2 = np.dot(X, theta)
     hypoY = 1 / (1 + np.exp(-t2))
 
-    #print Y.shape
-    #print t2.shape
-
     compY = np.append(np.reshape(Y, (Y.size, 1)), hypoY, axis=1)
+
     #print compY
 
-    #print hypoY
-    #thetaT = theta.transpose()
-    #print thetaT
-    #print thetaT.shape
-    #xM = np.asmatrix(X)
-    #thetaM = np.asmatrix(theta)
-
-    mask0 = Y <= 0
-    mask1 = Y > 0
+    mask0 = np.array(Y).flatten() <= 0
+    mask1 = np.array(Y).flatten() > 0
 
     plt.plot(X[mask0, 1], X[mask0, 2], 'rx')
     plt.plot(X[mask1, 1], X[mask1, 2], 'bo')
 
     xmin1 = np.min(X[:, 1])
     xmax1 = np.max(X[:, 1])
+
     x1 = np.linspace(xmin1, xmax1)
-    x2 = -(theta[0, 0] / theta[0, 2]) - (theta[0, 1] / theta[0, 2]) * x1
+
+    theta0 = theta[0, 0]
+    theta1 = theta[1, 0]
+    theta2 = theta[2, 0]
+    x2 = -(theta0 / theta2) - (theta1 / theta2) * x1
 
     plt.plot(x1, x2)
 
     plt.show()
-
 
 main()
