8b51b01b9d763ed27a072389fac35d35b12bf24e
Kan Xu <xukanxukan@gmail.com>
2017-05-16 22:56:58 -0700

    add batch process

--- ml/ml01.py
+++ ml/ml01.py
@@ -47,10 +47,37 @@ def test_func1(xa, ya, max_iters):
             t2 = t1 * xm[i]
             theta = theta + alpha * t2.transpose()
 
-
     #hypoY = 1.0 / (1 + np.exp(-(xm * theta)))
     #print hypoY
 
+    return theta
+
+def test_func1_batch(xa, ya, max_iters):
+    xm = np.matrix(xa)
+    ym = np.matrix(ya)
+
+    ym01 = np.where(ym > 0, ym, 0)
+
+    mm = xm.shape[0]
+    nn = xm.shape[1]
+
+    theta = np.ones((nn, 1))
+
+    alpha = 0.05
+    hypo = np.zeros((mm, 1))
+    t1 = np.zeros((mm, 1))
+    t2 = np.zeros((mm, nn))
+    t3 = np.zeros((1, nn))
+
+    for j in range(0, max_iters):
+        margins = xm * theta
+        hypo = 1.0 / (1.0 + np.exp(-margins))
+        t1 = ym01 - hypo
+        t2 = np.multiply(xm, t1)
+        t3 = np.sum(t2, axis=0)
+        theta = theta + alpha * t3.transpose()
+
+    #print hypo
     return theta
 
 # minimize (y - h(x))^2
@@ -87,7 +114,7 @@ def print_and_compare(X, Y, theta):
     error = np.absolute(ym01 - hypoY)
     error = np.where(error > 0.5, 1, 0)
     num_error = np.count_nonzero(error)
-    print compY
+    #print compY
     print num_error
 
 def main():
@@ -100,14 +127,17 @@ def main():
 
     # theta = np.matrix([[-2.6205, 0.7604, 1.1719]]), # result from anser
     thetaN, ll = newton_opt(X, Y, 20)
-    thetaS = test_func1(X, Y, 100)
+    thetaS = test_func1(X, Y, 1000)
+    thetaSB = test_func1_batch(X, Y, 100)
     thetaLS = test_func2(X, Y, 100)
 
     print thetaN
     print thetaS
+    print thetaSB
     print thetaLS
 
     print_and_compare(X, Y, thetaN)
+    print_and_compare(X, Y, thetaSB)
     print_and_compare(X, Y, thetaLS)
 
     mask0 = np.array(Y).flatten() <= 0
@@ -134,6 +164,14 @@ def main():
     xs2 = -(thetaS0 / thetaS2) - (thetaS1 / thetaS2) * x1
 
     plt.plot(x1, xs2)
+
+    thetaSB0 = thetaSB[0, 0]
+    thetaSB1 = thetaSB[1, 0]
+    thetaSB2 = thetaSB[2, 0]
+    xsb2 = -(thetaSB0 / thetaSB2) - (thetaSB1 / thetaSB2) * x1
+
+    plt.plot(x1, xsb2)
+
 
     thetaLS0 = thetaLS[0, 0]
     thetaLS1 = thetaLS[1, 0]
