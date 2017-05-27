12c646dbc53a5c4014783ae80665f9f3044a2341
Kan Xu <xukanxukan@gmail.com>
2017-05-27 16:31:25 -0700

    add newton

--- ml/ml01.py
+++ ml/ml01.py
@@ -14,7 +14,7 @@ def newton_opt(xa, ya, max_iters):
     ll = np.zeros((max_iters, 1))
 
     ## newton's method to minimize function.
-    for i in range(1, max_iters):
+    for i in range(0, max_iters):
         margins = np.multiply(ym, xm * theta)
         ll[i] = (1.0/mm) * np.sum(np.log(1 + np.exp(-margins)))
         probs = 1.0 / (1 + np.exp(margins))
@@ -34,7 +34,7 @@ def test_func1(xa, ya, max_iters):
     mm = xm.shape[0]
     nn = xm.shape[1]
 
-    theta = np.ones((nn, 1))
+    theta = np.zeros((nn, 1))
 
     alpha = 0.05
 
@@ -47,9 +47,6 @@ def test_func1(xa, ya, max_iters):
             t2 = t1 * xm[i]
             theta = theta + alpha * t2.transpose()
 
-    #hypoY = 1.0 / (1 + np.exp(-(xm * theta)))
-    #print hypoY
-
     return theta
 
 def test_func1_batch(xa, ya, max_iters):
@@ -61,9 +58,9 @@ def test_func1_batch(xa, ya, max_iters):
     mm = xm.shape[0]
     nn = xm.shape[1]
 
-    theta = np.ones((nn, 1))
+    theta = np.zeros((nn, 1))
 
-    alpha = 0.05
+    alpha = 0.2
     hypo = np.zeros((mm, 1))
     t1 = np.zeros((mm, 1))
     t2 = np.zeros((mm, nn))
@@ -80,8 +77,56 @@ def test_func1_batch(xa, ya, max_iters):
     #print hypo
     return theta
 
+def test_func2_batch(xa, ya, max_iters):
+    xm = np.matrix(xa)
+    ym = np.matrix(ya)
+
+    mm = xm.shape[0]
+    nn = xm.shape[1]
+
+    theta = np.zeros((nn, 1))
+
+    alpha = 0.2
+    hypo = np.zeros((mm, 1))
+    t1 = np.zeros((mm, 1))
+    t2 = np.zeros((mm, nn))
+    t3 = np.zeros((1, nn))
+
+    for j in range(0, max_iters):
+        margins = xm * theta
+        hypo = 2.0 / (1.0 + np.exp(-margins)) - 1.0
+        t1 = ym - hypo
+        t2 = np.multiply(xm, t1)
+        t3 = np.sum(t2, axis=0)
+        theta = theta + alpha * t3.transpose()
+
+    return theta
+
+# grad = 0.5 * sum((y-(2*h-1)).*x)
+# H = -1 * sum((h*(1-h))xj*xk^t)
+def test_func2_newton(xa, ya, max_iters):
+    xm = np.matrix(xa)
+    ym = np.matrix(ya)
+
+    mm = xm.shape[0]
+    nn = xm.shape[1]
+
+    theta = np.zeros((nn, 1))
+
+    ## newton's method to minimize function.
+    for i in range(0, max_iters):
+        margins = xm * theta
+        hypo = 1.0 / (1 + np.exp(-margins))
+        ll = ym - (hypo * 2 - 1)
+        grad1 = 0.5 * (xm.transpose() * ll)
+        H = -1.0 * (xm.transpose() * np.diag(np.array(np.multiply(hypo, (1-hypo))).flatten()) * xm)
+        theta = theta - linalg.inv(H) * grad1
+
+    return theta
+
+
 # minimize (y - h(x))^2
-def test_func2(xa, ya, max_iters):
+def test_func_ls(xa, ya, max_iters):
     xm = np.matrix(xa)
     ym = np.matrix(ya)
 
@@ -106,7 +151,7 @@ def test_func2(xa, ya, max_iters):
 
     return theta
 
-def print_and_compare(X, Y, theta):
+def print_and_compare(X, Y, theta, l):
     t2 = X * theta
     hypoY = 1 / (1 + np.exp(-t2))
     compY = np.append(np.reshape(Y, (Y.size, 1)), hypoY, axis=1)
@@ -115,7 +160,14 @@ def print_and_compare(X, Y, theta):
     error = np.where(error > 0.5, 1, 0)
     num_error = np.count_nonzero(error)
     #print compY
-    print num_error
+    print l, ': ', num_error, ', ', theta
+
+def plot_theta(x1, theta, color, l):
+    theta0 = theta[0, 0]
+    theta1 = theta[1, 0]
+    theta2 = theta[2, 0]
+    xls2 = -(theta0 / theta2) - (theta1 / theta2) * x1
+    plt.plot(x1, xls2, color, label=l)
 
 def main():
     x = np.loadtxt('logistic_x.txt')
@@ -126,19 +178,19 @@ def main():
     Y = y.reshape((y.size, 1))
 
     # theta = np.matrix([[-2.6205, 0.7604, 1.1719]]), # result from anser
-    thetaN, ll = newton_opt(X, Y, 20)
-    thetaS = test_func1(X, Y, 1000)
-    thetaSB = test_func1_batch(X, Y, 100)
-    thetaLS = test_func2(X, Y, 100)
+    thetaN, ll = newton_opt(X, Y, 100)
+    theta1 = test_func1(X, Y, 100)
+    theta1B = test_func1_batch(X, Y, 100)
+    thetaLS = test_func_ls(X, Y, 100)
+    theta2B = test_func2_batch(X, Y, 100)
+    theta2N = test_func2_newton(X, Y, 100)
 
-    print thetaN
-    print thetaS
-    print thetaSB
-    print thetaLS
-
-    print_and_compare(X, Y, thetaN)
-    print_and_compare(X, Y, thetaSB)
-    print_and_compare(X, Y, thetaLS)
+    print_and_compare(X, Y, thetaN, 'newton')
+    print_and_compare(X, Y, theta1, 'map 0-1')
+    print_and_compare(X, Y, theta1B, 'map 0-1 batch')
+    print_and_compare(X, Y, thetaLS, 'least square')
+    print_and_compare(X, Y, theta2B, 'orig batch')
+    print_and_compare(X, Y, theta2N, 'orig newton')
 
     mask0 = np.array(Y).flatten() <= 0
     mask1 = np.array(Y).flatten() > 0
@@ -151,35 +203,14 @@ def main():
 
     x1 = np.linspace(xmin1, xmax1)
 
-    thetaN0 = thetaN[0, 0]
-    thetaN1 = thetaN[1, 0]
-    thetaN2 = thetaN[2, 0]
-    xn2 = -(thetaN0 / thetaN2) - (thetaN1 / thetaN2) * x1
+    plot_theta(x1, thetaN, 'r', 'newton')
+    plot_theta(x1, theta1, 'g', 'map 0-1')
+    plot_theta(x1, theta1B, 'b', 'map 0-1 batch')
+    plot_theta(x1, thetaLS, 'y', 'least square')
+    plot_theta(x1, theta2B, 'c', 'orig batch')
+    plot_theta(x1, theta2N, 'm', 'orig newton')
 
-    plt.plot(x1, xn2)
-
-    thetaS0 = thetaS[0, 0]
-    thetaS1 = thetaS[1, 0]
-    thetaS2 = thetaS[2, 0]
-    xs2 = -(thetaS0 / thetaS2) - (thetaS1 / thetaS2) * x1
-
-    plt.plot(x1, xs2)
-
-    thetaSB0 = thetaSB[0, 0]
-    thetaSB1 = thetaSB[1, 0]
-    thetaSB2 = thetaSB[2, 0]
-    xsb2 = -(thetaSB0 / thetaSB2) - (thetaSB1 / thetaSB2) * x1
-
-    plt.plot(x1, xsb2)
-
-
-    thetaLS0 = thetaLS[0, 0]
-    thetaLS1 = thetaLS[1, 0]
-    thetaLS2 = thetaLS[2, 0]
-    xls2 = -(thetaLS0 / thetaLS2) - (thetaLS1 / thetaLS2) * x1
-
-    plt.plot(x1, xls2)
-
+    plt.legend()
     plt.show()
 
 main()
