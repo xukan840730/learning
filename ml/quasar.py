68d06c4dd5fde5a1fdff72ca7b595d83392527e2
Kan Xu <kanxu@Kans-MacBook.local>
2017-07-18 23:28:26 -0700

    continue

--- ml/quasar.py
+++ ml/quasar.py
@@ -26,6 +26,7 @@ def weighted_lin_reg(xa, ya, tau):
     raw_x = xm[:, 1:]
 
     thetaM = np.empty([nn, mm])
+    estY = np.empty([mm, 1])
 
     # should be for each sample
     for i in range(0, mm):
@@ -38,21 +39,16 @@ def weighted_lin_reg(xa, ya, tau):
 
         thetaI = linalg.solve(t1, t2)
         thetaM[:, i] = thetaI.flatten()
+        estY[i] = np.dot(xm) #unfinished
 
     return thetaM
 
-def main():
-    raw_data = np.loadtxt('quasar_train.csv', delimiter=',')
-    lambdas = raw_data[0, :]
-    train_qso = raw_data[1:, :]
-
-    train_data0 = train_qso[0, :]
-
+def problem1(lambdas, y):
     # pre process data
     t1 = np.ones((lambdas.shape[0], 1))
     x = lambdas.reshape(lambdas.size, 1)
     X = np.append(t1, x, axis=1)
-    Y = train_data0.reshape(train_data0.size, 1)
+    Y = y.reshape(y.size, 1)
 
     # linear regression:
     theta0 = lin_reg(X, Y)
@@ -67,12 +63,44 @@ def main():
         est_y_weighted[i] = np.dot(X[i, :], theta_weighted[:, i])
 
     # plotting
-    plt.plot(lambdas, train_data0, '+k', label='Raw Data')
+    plt.plot(lambdas, y, '+k', label='Raw Data')
     plt.plot(lambdas, est_y_lin_reg, 'r', label='Regression Line')
     plt.plot(lambdas, est_y_weighted, 'b', label='Weighted Regression')
 
     plt.legend()
     plt.show()
+
+def problem2(lambdas, qso):
+    smoothed = qso
+    tau = 5
+
+    mm = qso.shape[0]
+    nn = qso.shape[1]
+    t1 = np.ones((nn, 1))
+    x = lambdas.reshape(nn, 1)
+    X = np.append(t1, x, axis=1)
+
+    for ii in range(0, mm):
+        ytrain = qso[ii, :]
+        ytrain = ytrain.reshape(ytrain.size, 1)
+        theta = weighted_lin_reg(X, ytrain, tau)
+
+        #ysmoothed = np.empty(ytrain.shape)
+        #for jj in range(0, nn):
+        #    ysmoothed[jj] = np.dot(X[jj, :], theta[:, jj])
+
+        #smoothed[ii, :] = ysmoothed.transpose()
+        print("done:", ii)
+
+def main():
+    raw_data = np.loadtxt('quasar_train.csv', delimiter=',')
+    lambdas = raw_data[0, :]
+    train_qso = raw_data[1:, :]
+
+    train_data0 = train_qso[0, :]
+    #problem1(lambdas, train_data0)
+
+    problem2(lambdas, train_qso)
 
 # test module
 main()
