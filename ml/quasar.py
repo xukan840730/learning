b0578f3f42614d0ebf239d73c794c3834940d733
kanxu <xukanxukan@gmail.com>
2017-07-19 18:06:44 -0700

    improve

--- ml/quasar.py
+++ ml/quasar.py
@@ -3,24 +3,37 @@ import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 
 # linear regression by normal equation
-def lin_reg(xa, ya):
-    xm = np.matrix(xa)
+def linreg(xa, ya):
+    # pre process data, set first column to ones
+    t1 = np.ones((xa.shape[0], 1))
+    x = xa.reshape(xa.size, 1)
+    X = np.append(t1, x, axis=1)
+
+    xm = np.matrix(X)
     ym = np.matrix(ya)
 
     t1 = xm.transpose() * xm
     t2 = xm.transpose() * ym
 
     theta = linalg.solve(t1, t2)
-    return theta
+    est_y = X * theta
+    return est_y, theta
 
 # weighted linear regression
-def weighted_lin_reg(xa, ya, tau):
+def local_weighted_linreg(xa, ya, tau):
+    # pre process data, set first column to ones
+    t1 = np.ones((xa.shape[0], 1))
+    x = xa.reshape(xa.size, 1)
+    X = np.append(t1, x, axis=1)
+
     # wi = exp(-(x - xi)^2 / 2tau^2)
-    xm = np.matrix(xa)
+    xm = np.matrix(X)
     ym = np.matrix(ya)
 
     mm = xm.shape[0]
     nn = xm.shape[1]
+
+    assert mm == ym.shape[0]
 
     # raw x doesn't have the first column of ones
     raw_x = xm[:, 1:]
@@ -39,58 +52,43 @@ def weighted_lin_reg(xa, ya, tau):
 
         thetaI = linalg.solve(t1, t2)
         thetaM[:, i] = thetaI.flatten()
-        estY[i] = np.dot(xm) #unfinished
+        estY[i] = np.dot(xm[i, :], thetaI)
 
-    return thetaM
+    return estY, thetaM
 
 def problem1(lambdas, y):
-    # pre process data
-    t1 = np.ones((lambdas.shape[0], 1))
-    x = lambdas.reshape(lambdas.size, 1)
-    X = np.append(t1, x, axis=1)
     Y = y.reshape(y.size, 1)
 
     # linear regression:
-    theta0 = lin_reg(X, Y)
-    est_y_lin_reg = X * theta0
+    est_y_lr, theta_lr = linreg(lambdas, Y)
 
     # weighted linear regression:
     tau = 5.0
-    theta_weighted = weighted_lin_reg(X, Y, tau)
-    est_y_weighted = np.empty(Y.shape)
-
-    for i in range(0, lambdas.size):
-        est_y_weighted[i] = np.dot(X[i, :], theta_weighted[:, i])
+    est_y_weighted, theta_weighted = local_weighted_linreg(lambdas, Y, tau)
 
     # plotting
     plt.plot(lambdas, y, '+k', label='Raw Data')
-    plt.plot(lambdas, est_y_lin_reg, 'r', label='Regression Line')
+    plt.plot(lambdas, est_y_lr, 'r', label='Regression Line')
     plt.plot(lambdas, est_y_weighted, 'b', label='Weighted Regression')
-
-    plt.legend()
-    plt.show()
 
 def problem2(lambdas, qso):
     smoothed = qso
     tau = 5
 
-    mm = qso.shape[0]
-    nn = qso.shape[1]
-    t1 = np.ones((nn, 1))
-    x = lambdas.reshape(nn, 1)
-    X = np.append(t1, x, axis=1)
+    num_samples = qso.shape[0]
+    num_features = qso.shape[1]
 
-    for ii in range(0, mm):
+    for ii in range(0, num_samples):
         ytrain = qso[ii, :]
         ytrain = ytrain.reshape(ytrain.size, 1)
-        theta = weighted_lin_reg(X, ytrain, tau)
+        ysmoothed, theta = local_weighted_linreg(lambdas, ytrain, tau)
+        smoothed[ii, :] = ysmoothed.transpose()
+        print("sample:", ii, " is done!")
 
-        #ysmoothed = np.empty(ytrain.shape)
-        #for jj in range(0, nn):
-        #    ysmoothed[jj] = np.dot(X[jj, :], theta[:, jj])
-
-        #smoothed[ii, :] = ysmoothed.transpose()
-        print("done:", ii)
+    # plotting
+    for ii in range(0, 10):
+        l = str(ii)
+        plt.plot(lambdas, smoothed[ii, :], label=l)
 
 def main():
     raw_data = np.loadtxt('quasar_train.csv', delimiter=',')
@@ -98,9 +96,11 @@ def main():
     train_qso = raw_data[1:, :]
 
     train_data0 = train_qso[0, :]
-    #problem1(lambdas, train_data0)
-
+    problem1(lambdas, train_data0)
     problem2(lambdas, train_qso)
+
+    plt.legend()
+    plt.show()
 
 # test module
 main()
