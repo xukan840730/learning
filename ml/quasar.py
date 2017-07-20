22893471f1cfa260afe7066d11a615c38cdb3079
kanxu <xukanxukan@gmail.com>
2017-07-20 14:48:40 -0700

    finish chapter 1 homework

--- ml/quasar.py
+++ ml/quasar.py
@@ -72,7 +72,7 @@ def problem1(lambdas, y):
     plt.plot(lambdas, est_y_weighted, 'b', label='Weighted Regression')
 
 def smooth_qso(lambdas, qso, tau):
-    smoothed = qso
+    smoothed = np.empty(qso.shape)
 
     num_samples = qso.shape[0]
     num_features = qso.shape[1]
@@ -92,7 +92,7 @@ def ker(t):
     tt[idx] = 0.0
     return tt
 
-def problem3(lambdas, smoothed_qso):
+def estimate_left_qso(lambdas, smoothed_qso):
     rt_idx = lambdas >= 1300
     lt_idx = lambdas < 1200
     qso_right = smoothed_qso[:, rt_idx]
@@ -102,32 +102,74 @@ def problem3(lambdas, smoothed_qso):
 
     k = 3
 
+    est_qso_left = np.empty(qso_left.shape)
+
+    # estimate left qso
     for jj in range(0, mm):
         qso_diff = qso_right - qso_right[jj, :]
         metric_d = np.sum(qso_diff * qso_diff, axis=1)
         h = np.max(metric_d)
 
         sorted_idx = np.argsort(metric_d)
-        sorted_right = qso_right[sorted_idx]
+        #sorted_right = qso_right[sorted_idx]
         sorted_left = qso_left[sorted_idx]
-        neighb_qso = sorted_right[1:1+k, :]
+        #neighb_right = sorted_right[0:k, :]
+        neighb_left = sorted_left[0:k, :]
         sorted_diff = metric_d[sorted_idx]
-        neighb_diff = sorted_diff[1:1+k]
+        neighb_diff = sorted_diff[0:k]
 
-        t2 = np.sum(ker(neighb_diff / h))
+        ker_diff = ker(neighb_diff / h)
+        denom = np.sum(ker_diff)
 
-    print('done')
+        t2 = np.empty(neighb_left.shape)
+        for qq in range(0, ker_diff.size):
+            t2[qq, :] = neighb_left[qq, :] * ker_diff[qq]
+
+        t3 = np.sum(t2, axis=0)
+        f_left_hat = t3 / denom
+        #assert f_left_hat.shape[1] == est_qso_left.shape[1]
+        est_qso_left[jj, :] = f_left_hat
+
+    return qso_left, est_qso_left
+
+def calc_error(lambdas, qso_left, est_qso_left):
+    # calculate error
+    qso_error = qso_left - est_qso_left
+    t2 = qso_error * qso_error
+    metric_error = np.sum(t2, axis=1)
+    print("average error:", np.average(metric_error))
+
+def plot_est_left(lambdas, smoothed_qso, est_qso_left, figure_idx, plot_idx):
+    plt.figure(figure_idx)
+    plt.plot(lambdas, smoothed_qso[plot_idx, :], '+k')
+    plt.plot(lambdas[0:est_qso_left.shape[1]], est_qso_left[plot_idx, :].flatten(), 'r')
+    print("plot-done")
 
 def main():
-    raw_data = np.loadtxt('quasar_train.csv', delimiter=',')
-    lambdas = raw_data[0, :]
-    train_qso = raw_data[1:, :]
+    raw_data_train = np.loadtxt('quasar_train.csv', delimiter=',')
+    lambdas_train = raw_data_train[0, :]
+    train_qso = raw_data_train[1:, :]
 
-    train_data0 = train_qso[0, :]
+    raw_data_test = np.loadtxt('quasar_test.csv', delimiter=',')
+    lambdas_test = raw_data_test[0, :]
+    test_qso = raw_data_test[1:, :]
+
+    #train_data0 = train_qso[0, :]
     #problem1(lambdas, train_data0)
 
-    smoothed_train = smooth_qso(lambdas, train_qso, 5)
-    problem3(lambdas, smoothed_train)
+    tau = 5
+    smoothed_train = smooth_qso(lambdas_train, train_qso, tau)
+    smoothed_test = smooth_qso(lambdas_test, test_qso, tau)
+
+    qso_train_left, est_train_left = estimate_left_qso(lambdas_train, smoothed_train)
+    qso_test_left, est_test_left = estimate_left_qso(lambdas_test, smoothed_test)
+    calc_error(lambdas_train, qso_train_left, est_train_left)
+    calc_error(lambdas_test, qso_test_left, est_test_left)
+
+    # 1st figure is sample 0
+    plot_est_left(lambdas_test, smoothed_test, est_test_left, 1, 0)
+    # 2st figure is sample 5
+    plot_est_left(lambdas_test, smoothed_test, est_test_left, 2, 5)
 
     #plt.legend()
     plt.show()
