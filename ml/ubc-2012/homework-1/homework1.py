be146ab9b91418e115b93917b37077f928595d1e
kanxu <xukanxukan@gmail.com>
2017-12-26 10:00:38 -0800

    change homework-1

--- ml/ubc-2012/homework-1/homework1.py
+++ ml/ubc-2012/homework-1/homework1.py
@@ -1,66 +1,114 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-links = {}
+def html_to_links(fnames):
+    links = {}
+    for file in fnames:
+        links[file] = []
+        f = open(file)
+        for line in f.readlines():
+            while True:
+                temp = line.partition('<a href="http://')
+                p = temp[2]
+                if p == '':
+                    break
+                url, _, line = p.partition('\">')
+                links[file].append(url)
+
+        f.close()
+    return links
+
+def links_to_edges(links):
+    edges = []
+    for key, values in links.items():
+        eweight = {}
+        for v in values:
+            if v in eweight:
+                eweight[v] += 1
+            else:
+                eweight[v] = 1
+
+        for succ, weight in eweight.items():
+            edges.append([key, succ, {'weight': weight}])
+    return edges
+
 fnames = ['angelinajolie.html','bradpitt.html',
           'jenniferaniston.html','jonvoight.html',
           'martinscorcese.html','robertdeniro.html']
+num_fnames = len(fnames)
 
-for file in fnames:
-    links[file] = []
-    f = open(file)
-    for line in f.readlines():
-        while True:
-            temp = line.partition('<a href="http://')
-            p = temp[2]
-            if p == '':
-                break
-            url, _, line = p.partition('\">')
-            links[file].append(url)
+links = html_to_links(fnames)
+edges = links_to_edges(links)
 
-    f.close()
+def pagerank(fnames, edges):
+    NX = len(fnames)
+    T = np.matrix(np.zeros((NX, NX)))
 
-import networkx as nx
-DG = nx.DiGraph()
-DG.add_nodes_from(fnames)
-edges = []
-for key, values in links.items():
-    eweight = {}
-    for v in values:
-        if v in eweight:
-            eweight[v] += 1
-        else:
-            eweight[v] = 1
+    # f2i : fname to index
+    f2i = dict((fn, i) for i, fn in enumerate(fnames))
 
-    for succ, weight in eweight.items():
-        edges.append([key, succ, {'weight':weight}])
+    for e in edges:
+        T[f2i[e[0]], f2i[e[1]]] = e[2]['weight']
 
-DG.add_edges_from(edges)
+    epsilon = 0.01
+    # small scalar matrix so even tiny web still gets some weight
+    E = np.ones(T.shape) / NX
+    L = T + epsilon * E  # not normalized matrix
 
-plt.figure(figsize=(9,9))
-pos=nx.spring_layout(DG,iterations=10)
-nx.draw(DG,pos,alpha=0.4,edge_color='r',font_size=16,with_labels=True)
-plt.savefig('link_graph.png')
-plt.show()
+    G = np.matrix(np.zeros(L.shape))
+    for i in range(0, NX):
+        G[i, :] = L[i, :] / np.sum(L[i, :])
 
-#import cPickle as pickle
-#pickle.dump(DG, open('DG.pkl', 'w'))
+    return G
 
-import numpy as np
-NX = len(fnames)
-T = np.matrix(np.zeros((NX, NX)))
+def rand_norm_p(rank):
+    p = np.random.random(rank)
+    sum_p = np.sum(p)
+    return p / sum_p
 
-f2i = dict((fn, i) for i, fn in enumerate(fnames))
+G = pagerank(fnames, edges)
 
-for pred, succ in DG.adj.items():
-    for s, edata in succ.items():
-        T[f2i[pred], f2i[s]] = edata['weight']
+num_iter = 20
 
-E = np.ones(T.shape) / NX
-epsilon = 0.01
-L = T + epsilon * E
-G = np.matrix(np.zeros(L.shape))
-for i in range(0, NX):
-    G[i,:] = L[i,:] / np.sum(L[i,:])
+def evol(p, G, num_iter):
+    r1 = p.copy()
+    # r2 = p.copy()
+    G1 = G.copy()
+    evol1 = [np.dot(r1, G1**i) for i in range(1, num_iter + 1)]
+    # The same as the following:
+    # evol2 = []
+    # Gi = np.matrix(np.identity(G.shape[0]))
+    # for _ in range(1, num_iter + 1):
+    #     Gi = np.dot(Gi, G)
+    #     r = np.dot(r2, Gi)
+    #     evol2.append(r)
 
-print(G)
+    # print evol1
+    # print evol2
+    return evol1
+
+def plt_mat_g(num_fnames, G, num_iter):
+    p = rand_norm_p(num_fnames)
+    evolution = evol(p, G, num_iter)
+
+    x = np.array(range(1, num_iter + 1))
+
+    table = np.zeros((num_fnames, num_iter))
+
+    for i in range(0, num_fnames):
+        for j in range(0, num_iter):
+            table[i,j] = evolution[j][0, i] # evolution is a list of 1d matrix
+
+        plt.plot(x, table[i, :], label=fnames[i], lw=2)
+        #print table[i,:]
+
+    plt.title('rank vs iterations')
+    plt.xlabel('iterations')
+    plt.ylabel('rank')
+    plt.legend()
+    plt.show()
+
+# plot evolution
+plt_mat_g(num_fnames, G, num_iter)
+
