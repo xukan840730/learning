7389ac037b4e010d65f3e6ee5f4dc0a8848af7fd
kanxu <xukanxukan@gmail.com>
2017-12-27 12:53:53 -0800

    finish problem2-5

--- ml/ubc-2012/homework-1/homework2-5/homework2-5.py
+++ ml/ubc-2012/homework-1/homework2-5/homework2-5.py
@@ -1,33 +1,64 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import log
 from nltk import word_tokenize
+from nltk.stem.porter import PorterStemmer
 
 fnames = ['angelinajolie.html','bradpitt.html',
           'jenniferaniston.html','jonvoight.html',
           'martinscorcese.html','robertdeniro.html']
 num_fnames = len(fnames)
 
-x = "This (sentence) has punctuation."
-word_tokenize(x)
+fname_total_words = {}
+revind = {}
+stemmer = PorterStemmer()
 
-# def create_revise_dict(fnames):
-#     revind = {}
-#
-#     for fn in fnames:
-#         f = open(fn)
-#         for line in f.readlines():
-#             for token in word_tokenize(line):
-#                 if token in revind:
-#                     if fn in revind[token]:
-#                         revind[token][fn] += 1
-#                     else:
-#                         revind[token][fn] = 1
-#                 else:
-#                     revind[token] = {fn:1}
-#
-#     return revind
-#
-# revind = create_revise_dict(fnames)
+for fn in fnames:
+    fname_total_words[fn] = 0
+
+    f = open(fn)
+    for line in f.readlines():
+        words = word_tokenize(line)
+        for token in words:
+            wtoken = stemmer.stem(token)
+            if wtoken in revind:
+                if fn in revind[wtoken]:
+                    revind[wtoken][fn] += 1
+                else:
+                    revind[wtoken][fn] = 1
+            else:
+                revind[wtoken] = {fn:1}
+
+            fname_total_words[fn] += 1
+
+print fname_total_words
+print revind
+
+# term frequency-inverse document frequency
+# term frequency:
+#   (total number of times w appear in d) / (the total number of words in d)
+# inverse document frequency:
+#   log(number of documents in D / (1 + number of documents word w appears in)
+def tfidf(revind, fnames, fname_total_words, word):
+    print "tfidf for word:%s" % word
+    wstem = stemmer.stem(word)
+    if wstem != word:
+        print "word '%s' stemmed to '%s'" % (word, wstem)
+
+    t1 = 1.0
+    if wstem in revind:
+        t1 = 1.0 + len(revind[wstem])
+    idf = log(len(fnames) / t1)
+
+    if wstem in revind:
+        for fn, count in revind[wstem].items():
+            tf = float(count) / float(fname_total_words[fn])
+            tf_idf = tf * idf
+            print "TF-IDF for '%s', count:%d = %f" % (fn, count, tf_idf)
+
+tfidf(revind, fnames, fname_total_words, 'acting')
+tfidf(revind, fnames, fname_total_words, 'awards')
+tfidf(revind, fnames, fname_total_words, 'and')
 
 # def get_page_rank(fname):
 #     return r[0, f2i[fname]] # r is a matrix
