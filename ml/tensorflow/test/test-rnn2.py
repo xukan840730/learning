4335f833951189866b5c6652b5baa2e0bec157ce
Kan Xu <xukanxukan@gmail.com>
2018-05-13 19:13:27 -0700

    update test-rnn2

--- ml/tensorflow/test/test-rnn2.py
+++ ml/tensorflow/test/test-rnn2.py
@@ -143,22 +143,22 @@ with tf.Session() as session:
     print("optimization finished!")
     print("elapsed time: ", elapsed(time.time() - start_time))
 
-    while True:
-        prompt = "%s words: " % n_input
-        sentence = input(prompt)
-        sentence = sentence.strip()
-        words = sentence.split(' ')
-        if len(words) != n_input:
-            continue
-        try:
-            symbols_in_keys = [dictionary[str(words[i])] for i in range(len(words))]
-            for i in range(32):
-                keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
-                onehot_pred = session.run(pred, feed_dict={x:keys})
-                onehot_pred_index = int(tf.argmax(onehot_pred, 1).eval())
-                sentence = "%s %s" % (sentence, reverse_dictionary[onehot_pred_index])
-                symbols_in_keys = symbols_in_keys[1:]
-                symbols_in_keys.append(onehot_pred_index)
-            print(sentence)
-        except:
-            print("Word not in dictionary!")
+    # while True:
+    #     prompt = "%s words: " % n_input
+    #     sentence = input(prompt)
+    #     sentence = sentence.strip()
+    #     words = sentence.split(' ')
+    #     if len(words) != n_input:
+    #         continue
+    #     try:
+    #         symbols_in_keys = [dictionary[str(words[i])] for i in range(len(words))]
+    #         for i in range(32):
+    #             keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
+    #             onehot_pred = session.run(pred, feed_dict={x:keys})
+    #             onehot_pred_index = int(tf.argmax(onehot_pred, 1).eval())
+    #             sentence = "%s %s" % (sentence, reverse_dictionary[onehot_pred_index])
+    #             symbols_in_keys = symbols_in_keys[1:]
+    #             symbols_in_keys.append(onehot_pred_index)
+    #         print(sentence)
+    #     except:
+    #         print("Word not in dictionary!")
