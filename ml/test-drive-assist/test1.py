1f6e6742dc88085940f5bea87413ef77a7e8e39f
kanxu <xukanxukan@gmail.com>
2018-12-06 12:16:29 -0800

    udpate test-drive-assist project
    use smaller picture.

--- ml/test-drive-assist/test1.py
+++ ml/test-drive-assist/test1.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny = cv2.Canny(blur, 50, 150)
+    # canny = cv2.Canny(blur, 50, 150) ;; original value
+    canny = cv2.Canny(blur, 20, 40)
     return canny
 
-image = cv2.imread('test_image.jpg')
+image = cv2.imread('test_image_small.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 cv2.imshow("result", canny)
