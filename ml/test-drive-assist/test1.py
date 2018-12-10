3278eb60f3b0343b1eb4ae82f8a95e591c43b10d
kanxu <xukanxukan@gmail.com>
2018-12-09 22:04:54 -0800

    update test method.

--- ml/test-drive-assist/test1.py
+++ ml/test-drive-assist/test1.py
@@ -15,29 +15,31 @@ canny = canny(lane_image)
 
 canny_copy = np.copy(canny)
 
-cv2.imshow("result", canny_copy)
+# cv2.imshow("result", canny_copy)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# scaledown test: when scaled down to (60, 80), road can still be recognized
+# tiny_canny = cv2.resize(canny_copy, (0,0), fx=0.125, fy=0.125, interpolation=cv2.INTER_AREA)
+tiny_canny = cv2.resize(canny_copy, (0,0), fx=0.125, fy=0.125)
+
+
+title = "result_%d" % tiny_canny.shape[0]
+# cv2.imshow(title, tiny_canny)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+print(np.count_nonzero(tiny_canny))
+print(np.count_nonzero(tiny_canny > 64))
+print(np.count_nonzero(tiny_canny > 80))
+print(np.count_nonzero(tiny_canny > 96))
+print(np.count_nonzero(tiny_canny > 128))
+print(np.count_nonzero(tiny_canny > 240))
+
+tiny_canny2 = tiny_canny.copy()
+tiny_canny2[tiny_canny2 <= 128] = 0
+cv2.imshow(title, tiny_canny2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# scaledown test: when scaled down to (60, 80), road can still be recognized
-# while True:
-#     shape0, shape1 = canny_copy.shape
-#     new_shape = (shape0 // 2, shape1 // 2)
-#
-#     if new_shape[0] < 10 or new_shape[1] < 10:
-#         break
-#
-#     # cv2.resize(canny_copy, new_shape, new_canny, 0, 0, cv2.INTER_AREA)
-#     new_canny = cv2.resize(canny_copy, (0,0), fx=0.5, fy=0.5)
-#
-#     title = "result_%d" % shape0
-#     print(new_shape)
-#
-#     cv2.imshow(title, new_canny)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#
-#     canny_copy = new_canny
-
-# cv2.imshow("result", canny)
-# cv2.waitKey(0)
+# now we have a much simpler image to do line/curve fitting.
