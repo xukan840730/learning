4d6ba50144a76c1c7817cea3f6586e1a0d014997
kanxu <xukanxukan@gmail.com>
2018-12-10 19:50:03 -0800

    update project.

---
+++ ml/test-drive-assist/simple-math.py
@@ -0,0 +1,7 @@
+
+## need learn python class.
+
+# class vec2:
+
+# class line2d:
+    # a line has one point and one direction.
--- ml/test-drive-assist/test1.py
+++ ml/test-drive-assist/test1.py
@@ -23,7 +23,6 @@ canny_copy = np.copy(canny)
 # tiny_canny = cv2.resize(canny_copy, (0,0), fx=0.125, fy=0.125, interpolation=cv2.INTER_AREA)
 tiny_canny = cv2.resize(canny_copy, (0,0), fx=0.125, fy=0.125)
 
-
 title = "result_%d" % tiny_canny.shape[0]
 # cv2.imshow(title, tiny_canny)
 # cv2.waitKey(0)
@@ -36,10 +35,34 @@ print(np.count_nonzero(tiny_canny > 96))
 print(np.count_nonzero(tiny_canny > 128))
 print(np.count_nonzero(tiny_canny > 240))
 
-tiny_canny2 = tiny_canny.copy()
-tiny_canny2[tiny_canny2 <= 128] = 0
-cv2.imshow(title, tiny_canny2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# tiny_canny2 = tiny_canny.copy()
+# tiny_canny2[tiny_canny2 <= 128] = 0
+# cv2.imshow(title, tiny_canny2)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+def retrieve_sample_points(image, threshold):
+    num_points = np.count_nonzero(image > threshold)
+
+    # retrieve points coordinates
+    coords = np.zeros((num_points, 2))
+
+    idx = 0
+    for idx_i in range(image.shape[0]):
+        for idx_j in range(image.shape[1]):
+            if image[idx_i, idx_j] > threshold:
+                coords[idx] = np.array((idx_i, idx_j))
+                idx = idx + 1
+
+    return coords
+
 
 # now we have a much simpler image to do line/curve fitting.
+threshold = 128
+sample_points = retrieve_sample_points(tiny_canny, threshold)
+
+def generate_line_candidates(scatter_points):
+    lines = ()
+    return lines
+
+line_candidates = generate_line_candidates(sample_points)
