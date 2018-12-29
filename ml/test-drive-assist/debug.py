04b57e2d4155bc7e7e7ec3267ac861b4a0f51703
Kan Xu <xukanxukan@gmail.com>
2018-12-28 17:56:25 -0800

    prototype histogram

--- ml/test-drive-assist/debug.py
+++ ml/test-drive-assist/debug.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def debug_sobel(image_gray, sobel_hori_f, sobel_vert_f, threshold, image_histo_bins):
+def debug_sobel(image_gray, sobel_hori_f, sobel_vert_f, threshold):
     sobel_hori_b = np.zeros(sobel_hori_f.shape)
     sobel_hori_g = sobel_hori_b.copy()
     sobel_hori_r = sobel_hori_b.copy()
@@ -60,19 +60,29 @@ def debug_sobel(image_gray, sobel_hori_f, sobel_vert_f, threshold, image_histo_b
                     sobel_local_max_r[ix, iy] = 1.0
 
     # fill histogram bins color
-    for ix in range(sobel_hori_f.shape[0]):
-        for iy in range(sobel_hori_f.shape[1]):
-            k = image_histo_bins[ix, iy]
-            m = k % 4
-            if m == 0:
-                histo_bins_g[ix, iy] = 1.0
-            elif m == 1:
-                histo_bins_b[ix, iy] = 1.0
-            elif m == 2:
-                histo_bins_r[ix, iy] = 1.0
-            else:
-                histo_bins_r[ix, iy] = 1.0
-                histo_bins_g[ix, iy] = 1.0
+    # for ix in range(sobel_hori_f.shape[0]):
+    #     for iy in range(sobel_hori_f.shape[1]):
+    #         k = image_histo_bins[ix, iy]
+    #         m = k % 4
+    #         if m == 0:
+    #             histo_bins_g[ix, iy] = 1.0
+    #         elif m == 1:
+    #             histo_bins_b[ix, iy] = 1.0
+    #         elif m == 2:
+    #             histo_bins_r[ix, iy] = 1.0
+    #         elif m == 3:
+    #             histo_bins_r[ix, iy] = 1.0
+    #             histo_bins_g[ix, iy] = 1.0
+    #         elif m == 4:
+    #             histo_bins_r[ix, iy] = 1.0
+    #             histo_bins_b[ix, iy] = 1.0
+    #         elif m == 5:
+    #             histo_bins_g[ix, iy] = 1.0
+    #             histo_bins_b[ix, iy] = 1.0
+    #         elif m == 6:
+    #             histo_bins_g[ix, iy] = 1.0
+    #             histo_bins_b[ix, iy] = 1.0
+    #             histo_bins_r[ix, iy] = 1.0
 
     sobel_mag_mod = sobel_mag_f + sobel_local_max
     sobel_mag_mod_max = np.max(sobel_mag_mod)
@@ -92,7 +102,7 @@ def debug_sobel(image_gray, sobel_hori_f, sobel_vert_f, threshold, image_histo_b
     sobel_mag_dbg = cv2.merge((sobel_mag_b, sobel_mag_g, sobel_mag_r))
     sobel_mag_mod_dbg = cv2.merge((sobel_mag_mod_b, sobel_mag_mod_g, sobel_mag_mod_r))
     sobel_local_max_dbg = cv2.merge((sobel_local_max_b, sobel_local_max_g, sobel_local_max_r))
-    image_histo_dbg = cv2.merge((histo_bins_b, histo_bins_g, histo_bins_r))
+    # image_histo_dbg = cv2.merge((histo_bins_b, histo_bins_g, histo_bins_r))
 
     image_gray_3_u8 = cv2.cvtColor(image_gray, cv2.COLOR_GRAY2BGR)
     image_gray_3_f = image_gray_3_u8.astype(float) / 255.0
@@ -100,14 +110,14 @@ def debug_sobel(image_gray, sobel_hori_f, sobel_vert_f, threshold, image_histo_b
     sobel_hori_dbg2 = cv2.addWeighted(image_gray_3_f, 1.0, sobel_hori_dbg, 0.5, 0.0)
     sobel_vert_dbg2 = cv2.addWeighted(image_gray_3_f, 1.0, sobel_vert_dbg, 0.5, 0.0)
     sobel_mag_dbg2 = cv2.addWeighted(image_gray_3_f, 1.0, sobel_mag_dbg, 1.0, 0.0)
-    image_histo_dbg2 = cv2.addWeighted(image_gray_3_f, 1.0, image_histo_dbg, 0.5, 0.0)
+    # image_histo_dbg2 = cv2.addWeighted(image_gray_3_f, 1.0, image_histo_dbg, 0.5, 0.0)
 
     cv2.imshow("sobel_hori_dbg", sobel_hori_dbg2)
     cv2.imshow("sobel_vert_dbg", sobel_vert_dbg2)
     cv2.imshow("sobel_mag_dbg", sobel_mag_dbg)
     cv2.imshow("sobel_local_max", sobel_local_max_dbg)
     cv2.imshow("sobel_mag_mod_dbg", sobel_mag_mod_dbg)
-    cv2.imshow("image_histo_dbg", image_histo_dbg)
+    # cv2.imshow("image_histo_dbg", image_histo_dbg)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -142,10 +152,76 @@ def debug_expansion(image_gray, visited_mask, b, g, r):
     cv2.destroyAllWindows()
 
 def debug_histogram(image_gray_u8):
-    image_gray_hist = np.zeros((256), dtype=int)
+    colors_per_bin = 16
+    assert(256 % colors_per_bin == 0)
+    num_bins = 256 // colors_per_bin
+
+    image_gray_hist = np.zeros((num_bins), dtype=int)
     for ix in range(image_gray_u8.shape[0]):
-        k = image_gray_u8[ix]
+        k = image_gray_u8[ix] // colors_per_bin
         image_gray_hist[k] += 1
 
-    plt.hist(image_gray_u8, 256)
+    # print(np.std(image_gray_hist))
+
+    plt.hist(image_gray_u8, num_bins)
     plt.show()
+
+def proto_histogram(image_gray_u8):
+    num_bins = 16
+    assert (256 % num_bins == 0)
+    colors_per_bin = 256 // num_bins
+
+    # fill histogram content
+    image_gray_hist = np.zeros((num_bins), dtype=int)
+    for ix in range(image_gray_u8.shape[0]):
+        k = image_gray_u8[ix] // colors_per_bin
+        image_gray_hist[k] += 1
+
+    threshold = np.std(image_gray_hist) / 2.0 # magic number
+    ranges = list()
+
+    curr_bin_start = 0
+    while True:
+        while curr_bin_start < num_bins and image_gray_hist[curr_bin_start] == 0:
+            curr_bin_start += 1
+
+        if curr_bin_start == num_bins:
+            break
+
+        curr_bin_end = curr_bin_start
+        curr_bin_sum = image_gray_hist[curr_bin_start]
+        curr_bin_average = curr_bin_sum
+
+        while True:
+            next_bin_idx = curr_bin_end + 1
+            if next_bin_idx >= num_bins:
+                break
+
+            if abs(image_gray_hist[next_bin_idx] - curr_bin_average) > threshold:
+                break
+
+            curr_bin_end = next_bin_idx
+            curr_bin_sum += image_gray_hist[curr_bin_end]
+            curr_bin_average = curr_bin_sum / (curr_bin_end - curr_bin_start + 1)
+
+        ranges.append((curr_bin_start, curr_bin_end, curr_bin_sum, curr_bin_average))
+
+        # next iteration
+        curr_bin_start = curr_bin_end + 1
+        if curr_bin_start >= num_bins:
+            break
+
+    # prototype, merge single bin to others
+    while True:
+        found = False
+
+        # test middle one
+        for ix in ranges:
+            if ix[0] == ix[1]:
+                pass
+
+        # test side one
+
+        break
+
+    return ranges
