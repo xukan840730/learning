45e196e205b0cdd02ebd4c2e7fdb4da87c461b59
Kan Xu <xukanxukan@gmail.com>
2019-03-16 11:22:39 -0700

    capture one  hour.

--- ml/test-drive-assist/test_pi_capture.py
+++ ml/test-drive-assist/test_pi_capture.py
@@ -2,7 +2,7 @@ import time
 import datetime
 import os
 import picamera
-
+import cv2
 
 curtime = datetime.datetime.now()
 folder_name = "./capture-{0:d}-{1:d}-{2:d}-{3:d}-{4:d}".format(curtime.year, curtime.month, curtime.day, curtime.hour, curtime.minute)
@@ -13,12 +13,17 @@ if not os.path.exists(folder_name):
 os.chdir(folder_name)
 
 with picamera.PiCamera() as camera:
+    camera.resolution = (320, 240)
     camera.start_preview()
     try:
         for i, filename in enumerate(camera.capture_continuous('image{counter:04d}.jpg')):
             print(filename)
             time.sleep(1)
-            if i == 59:
+            if i == 3600: # one hour
+                break
+
+            key = cv2.waitKey(1)
+            if key > 0:
                 break
 
     finally:
