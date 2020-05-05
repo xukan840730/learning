ac3af06f8c2366535ad797ee79b3c9a78eac7cdf
kanxu <xukanxukan@gmail.com>
2020-05-05 16:22:59 -0700

    first-code.cpp with Eigen

---
+++ simple-ai/first-code/first-code.cpp
@@ -0,0 +1,29 @@
+// first-code.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <Eigen/Dense>
+
+typedef Eigen::Matrix<float, 3, 1> EVector;
+
+int main()
+{
+	EVector v1(1, 2, 3);
+	EVector v2(4, 5, 6);
+
+	EVector v3 = v1 + v2;
+
+    std::cout << v3; 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
