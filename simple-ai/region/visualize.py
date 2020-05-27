4fb6e2b8095579e2e812213d7de9568ba9fcd0e0
Kan Xu <xukanxukan@gmail.com>
2020-05-26 21:42:40 -0700

    need debug region phase 1.

--- simple-ai/region/visualize.py
+++ simple-ai/region/visualize.py
@@ -2,11 +2,45 @@ import numpy as np
 import cv2 as cv2
 
 def visualize_image_region(image_region):
-    img_dbg = np.zeros(image_region.shape, dtype=float)
+    channel_b = np.zeros(image_region.shape, dtype=float)
+    channel_g = np.zeros(image_region.shape, dtype=float)
+    channel_r = np.zeros(image_region.shape, dtype=float)
 
     for ix in range(image_region.shape[0]):
         for iy in range(image_region.shape[1]):
-            img_dbg[ix, iy] = int((image_region[ix, iy] % 9) / 9 * 255)
+            idx = image_region[ix, iy] % 9
+            if idx == 0:
+                channel_b[ix, iy] = 1.0
+                channel_g[ix, iy] = 1.0
+                channel_r[ix, iy] = 1.0
+            elif idx == 1:
+                channel_b[ix, iy] = 1.0
+            elif idx == 2:
+                channel_g[ix, iy] = 1.0
+            elif idx == 3:
+                channel_r[ix, iy] = 1.0
+            elif idx == 4:
+                channel_b[ix, iy] = 1.0
+                channel_g[ix, iy] = 1.0
+            elif idx == 5:
+                channel_b[ix, iy] = 1.0
+                channel_r[ix, iy] = 1.0
+            elif idx == 6:
+                channel_g[ix, iy] = 1.0
+                channel_r[ix, iy] = 1.0
+            elif idx == 7:
+                channel_b[ix, iy] = 1.0
+                channel_g[ix, iy] = 1.0
+                channel_r[ix, iy] = 1.0
+            elif idx == 8:
+                channel_b[ix, iy] = 0.0
+                channel_g[ix, iy] = 0.0
+                channel_r[ix, iy] = 0.0
+            else:
+                assert(False)
+
+
+    img_dbg = cv2.merge((channel_b, channel_g, channel_r))
 
     cv2.imshow('img_dbg', img_dbg)
     cv2.waitKey(0)
--- simple-ai/testing/test_region.py
+++ simple-ai/testing/test_region.py
@@ -7,7 +7,7 @@ def test1():
     img_u8 = cv2.imread('data\single\photo-road.jpg', cv2.IMREAD_COLOR)
     img_f = util.image_u8_to_f(img_u8)
 
-    size_small = (img_f.shape[1] // 4, img_f.shape[0] // 4)
+    size_small = (img_f.shape[1] // 8, img_f.shape[0] // 8)
     img_small_f = cv2.resize(img_f, dsize=size_small)
 
     cv2.imshow('img_small', img_small_f)
