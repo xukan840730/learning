e453ea802ece1604d6b908af21d810cd887124db
Kan Xu <xukanxukan@gmail.com>
2020-09-20 22:39:51 -0700

    update test_mnist_3.py

--- ml/misc/test_mnist_3.py
+++ ml/misc/test_mnist_3.py
@@ -13,6 +13,12 @@ valid_tensor_y = tensor(valid_raw_y).unsqueeze(1)
 
 print(valid_tensor_x.shape, valid_tensor_y.shape)
 
+def valid_tensor(t):
+    if (t!=t).any():
+        return False
+    else:
+        return True
+
 def mnist_loss(predictions, targets):
     predictions = predictions.sigmoid()
     targets_y = torch.zeros(predictions.shape)
@@ -23,6 +29,45 @@ def mnist_loss(predictions, targets):
 def mnist_loss_2(predictions, targets):
     # return torch.where(targets==1, 1-predictions, predictions).mean()
     return (predictions - targets).square().mean().sqrt()
+
+def mnist_loss_3(predictions, targets):
+    predictions = predictions.sigmoid()
+    targets_y = torch.zeros(predictions.shape)
+    for i in range(predictions.shape[0]):
+        targets_y[i][targets[i].item()] = 1.0
+
+    # use cross-entropy
+
+    # pred0 = predictions[0]
+    # targets0 = targets_y[0]
+    # print(pred0.shape, targets0.shape)
+    #
+    # tt = -(targets0 * torch.log(pred0) + (1-targets0)*torch.log(1-pred0))
+    # loss0 = tt.sum()
+
+    part1 = targets_y * predictions.log()
+    assert(valid_tensor(part1))
+    part2 = (1-targets_y) * (1-predictions).log()
+    if not valid_tensor(part2):
+        print("aaa")
+    tt1 = -(part1 + part2)
+    assert(valid_tensor(tt1))
+    tt2 = tt1.sum(dim=1)
+    assert(valid_tensor(tt2))
+    loss = tt2.mean()
+    assert(valid_tensor(loss))
+    return loss
+
+    # test_x = predictions[0:2]
+    # test_y = targets_y[0:2]
+    #
+    # part1 = test_y * torch.log(test_x)
+    # part2 = (1 - test_y) * torch.log(1-test_x)
+    #
+    # tt1 = -(part1 + part2)
+    # tt2 = torch.sum(tt1, dim=1)
+    # loss = tt2.mean()
+    # return loss
 
 def calc_grad(xb, yb, model):
     preds = model(xb)
@@ -37,7 +82,8 @@ def train_epoch(model, opt, dl):
 
 def batch_accuracy(xb, yb):
     preds = xb.sigmoid()
-    preds_x = torch.argmax(preds, dim=1)
+    preds_x = torch.argmax(preds, dim=1).unsqueeze(1)
+    assert(preds_x.shape == yb.shape)
     correct = preds_x == yb
     return correct.float().mean()
 
@@ -77,12 +123,12 @@ else:
     simple_net = nn.Sequential(
         nn.Linear(28 * 28, 30),
         nn.ReLU(),
-        nn.Linear(30, 20),
+        nn.Linear(30, 30),
         nn.ReLU(),
-        nn.Linear(20, 1),
+        nn.Linear(30, 10),
     )
     learn = Learner(dls, simple_net, opt_func=SGD,
-                    loss_func=mnist_loss_2, metrics=batch_accuracy_2)
+                    loss_func=mnist_loss_3, metrics=batch_accuracy)
 
-    lr = 0.01
-    learn.fit(50, lr=lr)
+    lr = 0.1
+    learn.fit(8, lr=lr)
