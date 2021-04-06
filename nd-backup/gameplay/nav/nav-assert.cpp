1e5e157f7af9b3bbd728d988caa671c1e87538e9
xukan840730 <xukanxukan@gmail.com>
2021-04-06 09:38:13 -0700

    Delete nd-backup directory

--- nd-backup/gameplay/nav/nav-assert.cpp
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (c) 2004 Naughty Dog, Inc.
- * A Wholly Owned Subsidiary of Sony Computer Entertainment, Inc.
- * Use and distribution without consent strictly prohibited.
- */
--- nd-backup/gameplay/nd-skin-manager.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2014 Naughty Dog, Inc. 
- * A Wholly Owned Subsidiary of Sony Computer Entertainment, Inc.
- * Use and distribution without consent strictly prohibited
- */
-
-#include "gamelib/gameplay/nd-skin-manager.h"
-
-NdSkinManager* g_pNdSkinManager = nullptr;
